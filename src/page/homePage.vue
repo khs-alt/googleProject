2568a594d3abf7c09d96968685434a9e537e2faa
khs-alt <aa_aa_aa_11@naver.com>
2023-11-29 21:16:00 +0900

    Remove / href

--- src/page/homePage.vue
+++ src/page/homePage.vue
@@ -6,13 +6,13 @@
                 <img :src="temp" style="max-width:100%; height: 50px; margin-right: auto; margin-left: 10px; float: left; margin-top: 8px;" />
             </div> -->
                 <div class="menu-content">
-                    <a href="/" style="margin-right: 10px;">
+                    <a href="" style="margin-right: 10px;">
                         <button class="signup-btn-style">Home</button>
                     </a>
-                    <a href="/signup" style="margin-right: 10px;">
+                    <a href="signup" style="margin-right: 10px;">
                         <button class="signup-btn-style">Sign Up</button>
                     </a>
-                    <a href="/admin">
+                    <a href="admin">
                         <button class="signup-btn-style">Admin</button>
                     </a>
                 </div>
@@ -111,7 +111,7 @@ export default {
         goToPage() {
             if (this.currentMode == 'Scoring') {
                 this.$router.push({
-                    path: '/scoring',
+                    path: 'scoring',
                     query: {
                         userName: this.userId,
                         currentPage: this.currentPage,
@@ -121,7 +121,7 @@ export default {
             }
             if (this.currentMode == 'Labeling') {
                 this.$router.push({
-                    path: '/label',
+                    path: 'label',
                     query: {
                         userName: this.userId,
                         currentPage: this.currentPage,
@@ -143,7 +143,7 @@ export default {
                     //console.log("user password: ", this.password)
                     // 여기에 아이디와 비밀번호를 전송하는 로직을 추가합니다.
                     axios
-                        .post(this.baseUrl + "/login", {
+                        .post(this.baseUrl + "login", {
                             current_mode: this.currentMode,
                             user_id: this.userId,
                             user_password: this.password,
