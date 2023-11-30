4ff2fd61a37e726a845e3efdab92e4da3d2bc78a
khs-alt <aa_aa_aa_11@naver.com>
2023-12-01 05:37:11 +0900

    add session axios function in main.js

--- src/page/scoringPage.vue
+++ src/page/scoringPage.vue
@@ -393,6 +393,8 @@ export default {
                 })
                 .catch((error) => {
                     console.log(error);
+                    alert("login failed")
+                    this.$router.push(process.env.BASE_URL);
                 })
         },
         navigateTo(item) {
