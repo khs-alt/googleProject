12cc20b445fcf97a806183b07102617b13d32e79
say7fish <BaguHo@gmail.com>
2023-12-04 23:39:42 +0900

    test

--- src/page/scoringPage.vue
+++ src/page/scoringPage.vue
@@ -13,9 +13,9 @@
     <div class="home-main-content" style="padding-bottom: 0; padding-top: 10px; margin-bottom: 8px;">
         <p style="font-size: 24px; margin-top: 10px">Video Ghosting Artifact Scoring System</p>
         <div style="display: flex;">
-            <!-- <div style="font-size: 20px; margin-left: auto; margin-right: 10px;">
-                {{ currentPageIndex }}/{{ totalLength }}
-            </div> -->
+            <div style="font-size: 20px; margin-left: auto; margin-right: 10px;">
+                <!-- {{ currentPageIndex }}/{{ totalLength }} -->
+            </div>
             <div style="margin-right: auto; margin-top: auto; margin-bottom: auto;" class="toggle-switch"
                 :class="{ 'active': isToggled }" @click="toggleVideo">
                 <div class="toggle-button" :style="{ left: isToggled ? '24px' : '0px' }"></div>
