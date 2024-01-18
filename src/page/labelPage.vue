bb7cfaf0d78dadf480151e2f5d2d9d04d23e2cf7
yh112 <msh7377@gmail.com>
2024-01-18 18:52:14 +0900

    modify checkProgressbar

--- src/page/labelPage.vue
+++ src/page/labelPage.vue
@@ -253,7 +253,7 @@ export default {
     checkProgressBar() {
       for (let i = 0; i < this.progressBarLength; i++) {
         for (let j = 0; j < this.progressBarList[i]; j++) {
-          console.log("[checkProgressBar] userLabelingList[" + (i * 100) + j + "]: " + this.userLabelingList[(i * 100) + j]);
+          console.log("[checkProgressBar] userLabelingList[" + (i * 100) + j + "]: " + this.userLabelingList[(i * 100) + j][0]);
           if (this.userLabelingList[(i * 100) + j][0] >= 0) {
             this.progressBarCount[i]++;
             console.log("[checkProgressBar] progressBarCount[" + i + "]: " + this.progressBarCount[i]);
