9da304a150ed55ea951fb1b94012526ea6e86367
yh112 <msh7377@gmail.com>
2024-01-18 19:32:29 +0900

    modify checkProgressbar

--- src/page/labelPage.vue
+++ src/page/labelPage.vue
@@ -146,7 +146,6 @@
 
 <script>
 import axios from 'axios'
-import { inject } from 'vue';
 export default {
   name: 'scoringPage',
   data() {
@@ -257,7 +256,7 @@ export default {
       for (let i = 0; i < this.progressBarLength; i++) {
         this.userLabelingCount = 0;
         for (let j = 0; j < this.progressBarList[i]; j++) {
-          console.log("[checkProgressBar] userLabelingList[" + int(i * 100) + int(j) + "]: " + this.userLabelingList[(i * 100) + j][0]);
+          console.log("[checkProgressBar] userLabelingList[" + Number(i * 100) + Number(j) + "]: " + this.userLabelingList[(i * 100) + j][0]);
           // console.log("[checkProgressBar] progressBarList[i]: " + this.progressBarList[i]);
           if (this.userLabelingList[(i * 100) + j][0] >= 0) {
             this.progressBarCount[i]++;
