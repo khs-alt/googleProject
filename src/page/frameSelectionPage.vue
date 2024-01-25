e78ad2419d47ce3bb31feda8d90e95720daaafc4
unknown <say7fish@gmail.com>
2024-01-25 12:07:39 +0900

    Remove video1CurrentTime in seekFroward

--- src/page/frameSelectionPage.vue
+++ src/page/frameSelectionPage.vue
@@ -469,8 +469,8 @@ export default {
           if (video1CurrentTime - videoFrame * 2 <= 0 || video2CurrentTime - videoFrame * 2 <= 0) {
             return;
           }
-          video1.currentTime = (parseFloat(video1CurrentTime) - parseFloat(videoFrame)).toFixed(2);
-          video2.currentTime = (parseFloat(video2CurrentTime) - parseFloat(videoFrame)).toFixed(2);
+          video1.currentTime -= parseFloat(videoFrame).toFixed(2);
+          video2.currentTime -= parseFloat(videoFrame).toFixed(2);
         }
       }
       console.log("[seekForward] video1.currentTime: ", video1.currentTime);
@@ -479,17 +479,17 @@ export default {
     async seekForward() {
       const video1 = this.$refs.videoNoartifact;
       const video2 = this.$refs.videoYesartifact;
+      const videoFrame = (Math.round((1 / this.videoFrameList[this.videoNameIndex]) * 100) / 100).toFixed(2);
       const video1CurrentTime = (Math.round((1 / video1.currentTime) * 100) / 100).toFixed(2);
       const video2CurrentTime = (Math.round((1 / video2.currentTime) * 100) / 100).toFixed(2);
-      const videoFrame = (Math.round((1 / this.videoFrameList[this.videoNameIndex]) * 100) / 100).toFixed(2);
 
       if (videoFrame != 0) {
         if (videoFrame) {
           if (video1CurrentTime + videoFrame * 2 >= video1.duration || video2CurrentTime + videoFrame * 2 >= video1.duration) {
             return;
           }
-          video1.currentTime = (parseFloat(video1CurrentTime) + parseFloat(videoFrame)).toFixed(2);
-          video2.currentTime = (parseFloat(video2CurrentTime) + parseFloat(videoFrame)).toFixed(2);
+          video1.currentTime += parseFloat(videoFrame).toFixed(2);
+          video2.currentTime += parseFloat(videoFrame).toFixed(2);
         }
       }
       console.log("[seekForward] video1CurrentTime: ", video1CurrentTime);
