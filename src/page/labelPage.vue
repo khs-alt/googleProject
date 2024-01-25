f86735615897dc73893c45901d4bca1dbe3f3287
yh112 <msh7377@gmail.com>
2024-01-25 22:41:11 +0900

    Add getImageIndexCurrentPage in create

--- src/page/labelPage.vue
+++ src/page/labelPage.vue
@@ -244,11 +244,12 @@ export default {
   },
 
   created() {
+    this.getImageIndexCurrentPage();
   },
 
   mounted() {
     window.addEventListener('keydown', this.keydown);
-    this.getImageIndexCurrentPage();
+    // this.getImageIndexCurrentPage();
     this.getUserLabeling();
     this.getImageNameList();
     this.checkProgressBar();
