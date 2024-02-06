cd7594fa531f178fede8686db9db2b66f07a229e
BaguHo <say7fish@gmail.com>
2024-02-06 18:06:28 +0900

    Remove + 1 in const t

--- src/page/frameSelectionPage.vue
+++ src/page/frameSelectionPage.vue
@@ -276,7 +276,7 @@ export default {
       // const videoTime = String(Math.round(this.videoCurrentTime * 100) / 100)
       const videoFrame = String(this.currentFrame)
       var video = document.getElementById('videoNoartifact');
-      const t = String(Math.round(((~~(video.currentTime / parseFloat(this.T) + 1) * this.T) + this.halfVideoFrameRate) * 100) / 100);
+      const t = String(Math.round(((~~(video.currentTime / parseFloat(this.T)) * this.T) + this.halfVideoFrameRate) * 100) / 100);
 
       if (!this.selectedVideoFrameList.includes(videoFrame)) {
         this.selectedVideoTimeList.push(t);
