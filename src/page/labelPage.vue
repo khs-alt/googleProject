91770592eb43c2d512c0f98592bf95e6cdfe8384
yh112 <msh7377@gmail.com>
2024-02-19 23:04:18 +0900

    Modify getImageSzie

--- src/page/labelPage.vue
+++ src/page/labelPage.vue
@@ -662,6 +662,8 @@ export default {
       return new Promise((resolve, reject) => {
         let img = new Image();
         img.src = this.serveOriginalImage();
+        img.src = this.serveArtifactImage();
+        img.src = this.serveDifferenceImage();
 
         let self = this;
         img.onload = function () {
