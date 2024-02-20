d53437e737442a4f7484e96e87940015a69a695b
say7fish <BaguHo@gmail.com>
2024-02-20 14:50:26 +0900

    test

--- src/page/labelPage.vue
+++ src/page/labelPage.vue
@@ -485,49 +485,16 @@ export default {
     },
 
     serveOriginalImage() {
-      return String(this.baseUrl + "postimage/original/" + (this.currentPage))
+      return require(this.baseUrl + "postimage/original/" + (this.currentPage))
     },
 
     serveArtifactImage() {
-      return String(this.baseUrl + "postimage/artifact/" + (this.currentPage))
+      return require(this.baseUrl + "postimage/artifact/" + (this.currentPage))
     },
 
     serveDifferenceImage() {
-      return String(this.baseUrl + "postimage/difference/" + (this.currentPage))
+      return require(this.baseUrl + "postimage/difference/" + (this.currentPage))
     },
-
-    // async preloadImage() {
-    //   if (this.currentPage === 0) {
-    //     this.nextOriginalImage = new Image();
-    //     this.nextArtifactImage = new Image();
-    //     this.nextDifferenceImage = new Image();
-    //     this.nextOriginalImage.src = String(this.baseUrl + "postimage/original/" + (this.currentPage + 1));
-    //     this.nextArtifactImage.src = String(this.baseUrl + "postimage/artifact/" + (this.currentPage + 1));
-    //     this.nextDifferenceImage.src = String(this.baseUrl + "postimage/difference/" + (this.currentPage + 1));
-    //   }
-    //   else if (this.currentPage === this.imageIndexList.length - 1) {
-    //     this.prevOriginalImage = new Image();
-    //     this.prevArtifactImage = new Image();
-    //     this.prevDifferenceImage = new Image();
-    //     this.prevOriginalImage.src = String(this.baseUrl + "postimage/original/" + (this.currentPage - 1));
-    //     this.prevArtifactImage.src = String(this.baseUrl + "postimage/artifact/" + (this.currentPage - 1));
-    //     this.prevDifferenceImage.src = String(this.baseUrl + "postimage/difference/" + (this.currentPage - 1));
-    //   }
-    //   else {
-    //     this.nextOriginalImage = new Image();
-    //     this.nextArtifactImage = new Image();
-    //     this.nextDifferenceImage = new Image();
-    //     this.nextOriginalImage.src = String(this.baseUrl + "postimage/original/" + (this.currentPage + 1));
-    //     this.nextArtifactImage.src = String(this.baseUrl + "postimage/artifact/" + (this.currentPage + 1));
-    //     this.nextDifferenceImage.src = String(this.baseUrl + "postimage/difference/" + (this.currentPage + 1));
-    //     this.prevOriginalImage = new Image();
-    //     this.prevArtifactImage = new Image();
-    //     this.prevDifferenceImage = new Image();
-    //     this.prevOriginalImage.src = String(this.baseUrl + "postimage/original/" + (this.currentPage - 1));
-    //     this.prevArtifactImage.src = String(this.baseUrl + "postimage/artifact/" + (this.currentPage - 1));
-    //     this.prevDifferenceImage.src = String(this.baseUrl + "postimage/difference/" + (this.currentPage - 1));
-    //   }
-    // },
     // Backend에서 patch size(행렬) 가져오는 method
     async getImageIndexCurrentPage() {
       let temp = String(this.currentPage);
@@ -869,7 +836,6 @@ export default {
       }
       else {
         this.postUserLabeling(-1);
-        this.imageIndex -= 1;
         this.i = 0;
         this.j = 0;
         this.currentPage = this.imageIndexList[this.imageIndex];
