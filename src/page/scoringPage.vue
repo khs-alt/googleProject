360413e228dc6195ce540de0367f8bf27424e0fb
say7fish <BaguHo@gmail.com>
2024-02-21 15:06:18 +0900

    Remove setting all of videos currentTime

--- src/page/scoringPage.vue
+++ src/page/scoringPage.vue
@@ -869,10 +869,10 @@ export default {
       var video2 = document.getElementById('videoYesartifact');
       var video3 = document.getElementById('toggleVideo');
 
-      var video1_currentTime = video1.currentTime;
-      video1.currentTime = video1_currentTime;
-      video2.currentTime = video1_currentTime;
-      video3.currentTime = video1_currentTime;
+      // var video1_currentTime = video1.currentTime;
+      // video1.currentTime = video1_currentTime;
+      // video2.currentTime = video1_currentTime;
+      // video3.currentTime = video1_currentTime;
 
       video1.addEventListener("play", () => {
         document.getElementById('videoYesartifact').play();
