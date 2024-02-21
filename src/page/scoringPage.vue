2415183fbfd150a836b044c48039a0dad5c96c50
BaguHo <say7fish@gmail.com>
2024-02-21 20:04:22 +0900

    Add console.log in video pause

--- src/page/scoringPage.vue
+++ src/page/scoringPage.vue
@@ -875,7 +875,7 @@ export default {
       // video3.currentTime = video1_currentTime;
 
       video1.addEventListener("play", () => {
-        if (video1.paused) {
+        if (!video1.paused) {
           console.log("[play paused]: " + video1.paused)
           document.getElementById('videoYesartifact').play();
           document.getElementById('toggleVideo').play();
@@ -885,7 +885,7 @@ export default {
       });
 
       video1.addEventListener("paused", () => {
-        if (!video1.paused) {
+        if (video1.paused) {
           console.log("[paused paused]: " + video1.paused)
           document.getElementById('videoYesartifact').pause();
           document.getElementById('toggleVideo').pause();
