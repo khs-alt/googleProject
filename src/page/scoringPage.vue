b7034cb5ce93b1989a05278845fdccaca88ce5f7
say7fish <BaguHo@gmail.com>
2024-02-22 17:49:11 +0900

    Fix laebl to label

--- src/page/scoringPage.vue
+++ src/page/scoringPage.vue
@@ -857,7 +857,7 @@ export default {
         this.currentPage = this.videoIndex[this.videoNameIndex];
         this.isPressed = [false, false, false, false, false, false];
         this.$router.push({
-          path: "/laebl/scoring",
+          path: "/label/scoring",
           query: {
             currentPage: this.currentPage,
             userName: this.currentUser,
